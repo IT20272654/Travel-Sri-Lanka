2c7f25a6e102dcf6542edf89d92aa23bd42bb02f
IT20272654 <shavindurajapaksha@gmail.com>
2024-05-15 14:31:28 +0530

    Updated Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container, Row, Button} from 'reactstrap'
 import { NavLink, Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
+import './Header.css'
 
 const nav_links = [
   {
@@ -9,7 +10,7 @@ const nav_links = [
     display:'Home'
   },
   {
-    path:'/#',
+    path:'/about',
     display:'About'
   },
   {
@@ -33,11 +34,29 @@ const Header = () => {
               {/* ========Menu Start======== */}
                 <div className='navigation'>
                   <ul className="menu d-flex align-items-center gap-5">
-
+                    {
+                      nav_links.map((item,index)=>(
+                        <li className="nav__item" key={index}>
+                          <NavLink to = {item.path} className={navClass=>navClass.isActive ? 'active__link' : ""}>
+                            {item.display}
+                          </NavLink>
+                        </li>
+                      ))
+                    }
                   </ul>
                 </div>
               {/* ========Menu End======== */}
 
+                <div className='nav_right d-flex align-items-center gap-4'>
+                    <div className='nav_btns d-flex align-items-center gap-4'>
+                      <Button className="btn secondary__btn"><Link to='/login'>Login</Link>
+                      </Button>
+                      <Button className="btn primary__btn"><Link to='/register'>Register</Link>
+                      </Button>
+                    </div>
+                    <span className="mobile__menu"></span>
+                    <i class="ri-menu-line"></i>
+                </div>     
 
           </div>
         </Row>
